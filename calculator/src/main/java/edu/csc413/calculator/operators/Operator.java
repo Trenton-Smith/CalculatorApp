9f97f31df7dd077d9ae7a6051f2c7d01c6b45d70
Trenton-Smith <54561063+Trenton-Smith@users.noreply.github.com>
2020-02-14 02:42:30 -0800

    Added operator subclasses as well

--- calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
@@ -2,6 +2,8 @@ package edu.csc413.calculator.operators;
 
 import edu.csc413.calculator.evaluator.Operand;
 
+import java.util.HashMap;
+
 public abstract class Operator {
     // The Operator class should contain an instance of a HashMap
     // This map will use keys as the tokens we're interested in,
@@ -15,10 +17,25 @@ public abstract class Operator {
     // operators.put( "-", new SubtractionOperator() );
 
     /**
+     * creates the HashMap with key/value pairs where in this case the values are subclasses of abstract class Operator
+     * static is able to be used here because the map is declared in the class-level scope, not in one of its methods
+     */
+    static HashMap operators = new HashMap();
+    static{
+        operators.put(")", new RightFlag() );
+        operators.put("+", new AddOperator() );
+        operators.put("-", new SubtractOperator() );
+        operators.put("*", new MultiplyOperator() );
+        operators.put("/", new DivideOperator() );
+        operators.put("^", new PowerOperator() );
+        operators.put("(", new LeftFlag() );//unsure if this is the right solution yet
+    }
+
+    /**
      * retrieve the priority of an Operator
      * @return priority of an Operator as an int
      */
-    public abstract int priority();
+    public abstract int priority();//this method will be overridden in each subclass to return the proper int value
 
     /**
      * Abstract method to execute an operator given two operands.
@@ -26,17 +43,8 @@ public abstract class Operator {
      * @param operandTwo second operand of operator
      * @return an operand of the result of the operation.
      */
-    public abstract Operand execute(Operand operandOne, Operand operandTwo);
-
-    /**
-     * determines if a given token is a valid operator.
-     * please do your best to avoid static checks
-     * for example token.equals("+") and so on.
-     * Think about what happens if we add more operators.
-     */
-    public static Operator getOperator(String token) {
-        return null;
-    }
+    public abstract Operand execute(Operand operandOne, Operand operandTwo);//this method will be overridden to perform each subclass'...
+                                                                            //...specific function
 
     /**
      * used to retrieve an operator from our HashMap.
@@ -46,7 +54,17 @@ public abstract class Operator {
      * @param token key of the operator we want to retrieve
      * @return reference to a Operator instance.
      */
+    public static Operator getOperator(String token) {
+        return (Operator) operators.get(token);//essentially works as our HashMap's getMethod
+    }
+
+    /**
+     * determines if a given token is a valid operator.
+     * please do your best to avoid static checks
+     * for example token.equals("+") and so on.
+     * Think about what happens if we add more operators.
+     */
     public static boolean check(String token) {
-        return false;
+        return operators.containsKey(token);//containsKey returns TRUE if token is a key corresponding to a value in the HashMap
     }
 }
---
+++ calculator/src/main/java/edu/csc413/calculator/operators/PowerOperator.java
@@ -0,0 +1,19 @@
+package edu.csc413.calculator.operators;
+
+import edu.csc413.calculator.evaluator.Operand;
+
+public class PowerOperator extends Operator{
+    @Override
+    public int priority() {
+        return 3;
+    }
+
+    @Override
+    public Operand execute(Operand operandOne, Operand operandTwo) {
+        int finalNum = 1;
+        for(int i=0; i < operandTwo.getValue();i++) {
+            finalNum *= operandOne.getValue();
+        }
+        return new Operand(finalNum);
+    }
+}
